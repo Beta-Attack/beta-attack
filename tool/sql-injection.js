ca8690e65295b78e7f417e36b693f60de55465ed
GianIsAlive <gianthedesigner@gmail.com>
2017-09-17 22:28:34 -0700

    included jsdom for easier dom access

---
+++ tool/sql-injection.js
@@ -0,0 +1,29 @@
+const Horseman = require('node-horseman');
+const jsdom = require('jsdom');
+const checklist = require('../checklist');
+const request = require('request');
+
+const horseman = new Horseman();
+const { JSDOM } = jsdom;
+
+const PORT = null
+const URI = `https://www.adafruit.com/${PORT}`;
+// Browser Engines:
+// - Mozilla/5.0 (Windows NT 6.1; WOW64)
+// - AppleWebKit/537.36 (KHTML, like Gecko)
+// - Chrome/37.0.2062.120 Safari/537.36
+
+horseman
+  .userAgent('Mozilla/5.0 Chrome/37.0.2062.120 AppleWebKit/537.36')
+  .open(URI)
+  .html()
+  .then((body) => {
+    const dom = new JSDOM(body);
+    const inputTag = dom.window.document.getElementsByTagName('input');
+    console.log('***************This is input***************');
+    for (let x = 0; x < inputTag.length; x += 1) {
+      console.log('This is a tag\'s link: ', inputTag[x].getAttribute('href'));
+    }
+    console.log('****************************************************');
+  })
+  .close();
