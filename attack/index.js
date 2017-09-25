76fc2c0b896efd9f1b760cd158d9895a1317ec31
GianIsAlive <gianthedesigner@gmail.com>
2017-09-25 10:58:44 -0700

    Inject dynamic xss script into the fields

--- attack/index.js
+++ attack/index.js
@@ -1,11 +1,12 @@
 const xssInjection = require('./xss-injection.js');
 const xssScripts = require('./xss-scripts.js');
-const config = require('./config.js');
 const jsdom = require('jsdom');
 const request = require('request');
 
 const { JSDOM } = jsdom;
+
 const attack = {};
+attack.url = null;
 
 const getForm = url =>
   new Promise((resolve, reject) => {
@@ -30,12 +31,14 @@ const getForm = url =>
 
 attack.injectFormInput = async (url) => {
   const result = [];
+  const XSSScripts = xssScripts.scripts(url);
+  console.log('This is XSSScripts: ', XSSScripts);
   const inputFields = await getForm(url);
   async function traverseInputs(i = inputFields.length - 1) {
     let index = i;
     if (index < 0) return;
     await xssInjection
-      .targetFormInput(xssScripts, inputFields[0])
+      .targetFormInput(XSSScripts, url, inputFields[0])
       .then((response) => {
         result.push(...response);
       });
@@ -45,9 +48,5 @@ attack.injectFormInput = async (url) => {
   await traverseInputs();
   return result;
 };
-
-// attack.injectFormInput(config.url).then((result) => {
-//   console.log('This is the result: ', result);
-// });
 
 module.exports = attack;
